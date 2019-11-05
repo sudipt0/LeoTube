0f57e0dce4223f0c60947ae30e37964d4802c34c
Sudipta Kumar Mondal <sudipta@Sudipta.local>
2019-11-05 19:04:58 +0530

    Initial setup

---
+++ app/User.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Str;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    public $incrementing = false;
+
+    protected static function boot(){
+        parent::boot();
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = (string) Str::uuid();
+        });
+    }
+
+    public function channel(){
+        return $this->hasOne(Channel::class);
+    }
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
